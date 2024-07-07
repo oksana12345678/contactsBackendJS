79f9b91eaabfbbe3b39ba80514295609ef93b0c2
Oksana Tanashchuk <oksana.tanashuk@gmail.com>
2024-07-07 11:39:26 +0200

    fix-status

--- src/server.js
+++ src/server.js
@@ -26,6 +26,7 @@ const setupServer = () => {
   app.get('/contacts', async (req, res) => {
     const contacts = await getAllContacts();
     res.status(200).json({
+      status: 200,
       message: 'Successfully found contacts!',
       data: contacts,
     });
@@ -40,6 +41,7 @@ const setupServer = () => {
       return;
     }
     res.status(200).json({
+      status: 200,
       message: `Successfully found contact with id {contactId}!`,
       data: contact,
     });
