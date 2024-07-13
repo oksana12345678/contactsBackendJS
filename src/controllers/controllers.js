bf6f8d6710a247c58c34d2619a89e44889233449
Oksana Tanashchuk <oksana.tanashuk@gmail.com>
2024-07-13 15:58:19 +0200

    Different request types traning   :: fixed getContactByIdController success message

--- src/controllers/controllers.js
+++ src/controllers/controllers.js
@@ -31,7 +31,7 @@ export const getContactByIdController = async (req, res, next) => {
 
     res.json({
       status: 200,
-      message: 'Successfully found contact with id ${contactId}',
+      message: `Successfully found contact with id ${contactId}`,
       data: contact,
     });
   } catch (err) {
