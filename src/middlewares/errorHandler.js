23d51de4b8b662662908d2e186eb12384fd1b132
Oksana Tanashchuk <oksana.tanashuk@gmail.com>
2024-07-14 14:54:33 +0200

    add

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -1,6 +1,6 @@
 import { HttpError } from 'http-errors';
 
-const errorHandler = (err, req, res) => {
+const errorHandler = (err, req, res, next) => {
   if (err instanceof HttpError) {
     res.status(err.status).json({
       status: err.status,
--- src/middlewares/notFoundHandler.js
+++ src/middlewares/notFoundHandler.js
@@ -1,4 +1,4 @@
-const notFoundHandler = (req, res) => {
+const notFoundHandler = (req, res, next) => {
   res.status(404).json({
     status: 404,
     message: 'route not found!',
