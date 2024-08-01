f558a6e1af0334d15b617ebc008070c8e5b8c1ea
Oksana Tanashchuk <oksana.tanashuk@gmail.com>
2024-08-01 14:10:47 +0200

    fix validate id middleware

--- src/middlewares/isValidId.js
+++ src/middlewares/isValidId.js
@@ -1,10 +1,9 @@
 import { isValidObjectId } from 'mongoose';
 import createHttpError from 'http-errors';
-
 const isValidId = (req, res, next) => {
-  const { id } = req.params;
+  const { contactId } = req.params;
 
-  if (!isValidObjectId(id)) {
+  if (!isValidObjectId(contactId)) {
     throw createHttpError(404, 'Not found');
   }
   next();
