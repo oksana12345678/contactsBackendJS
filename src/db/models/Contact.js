51eb5b45c4fa651eeed4f7a85f87db44eb2dc86a
Oksana Tanashchuk <oksana.tanashuk@gmail.com>
2024-08-01 14:17:22 +0200

    fix import

--- src/db/models/Contact.js
+++ src/db/models/Contact.js
@@ -32,4 +32,6 @@ const contactSchema = new Schema(
   },
 );
 
-export const Contact = model('contacts', contactSchema);
+const Contact = model('contacts', contactSchema);
+
+export default Contact;
