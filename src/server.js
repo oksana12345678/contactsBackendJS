ed958a302eacf3a1812a5eb8a7ec973fd96da3c0
Oksana Tanashchuk <oksana.tanashuk@gmail.com>
2024-09-23 13:07:27 +0200

    cors settings test 5

--- src/server.js
+++ src/server.js
@@ -17,30 +17,12 @@ const PORT = Number(env('PORT', '3000'));
 const setupServer = () => {
   const app = express();
 
-  //TODO check this working or not
-  app.use(function (req, res, next) {
-    res.header(
-      'Access-Control-Allow-Origin',
-      'https://phone-book-kohl.vercel.app',
-    );
-    res.header('Access-Control-Allow-Credentials', 'true');
-    res.header(
-      'Access-Control-Allow-Methods',
-      'GET,HEAD,PUT,PATCH,POST,DELETE',
-    );
-    res.header(
-      'Access-Control-Allow-Headers',
-      'Origin, X-Requested-With, Content-Type, Accept, Authorization',
-    );
-    next();
-  });
-
   const corsOptions = {
     origin: 'https://phone-book-kohl.vercel.app',
     credentials: true,
   };
   app.use(cors(corsOptions));
-  
+
   app.use((req, res, next) => {
     console.log('Cookies:', req.cookies);
     next();
